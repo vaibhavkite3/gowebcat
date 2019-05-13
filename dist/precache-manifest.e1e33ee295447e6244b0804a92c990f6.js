2f74ac6063d21d326f2c58c71690a269f8caf9cc
Unknown <vaibhavkite3@gmail.com>
2019-05-13 14:52:11 +0530

    Dist commit

---
+++ dist/precache-manifest.e1e33ee295447e6244b0804a92c990f6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e78ab56f9afb86597373",
+    "url": "/css/app.58e16b46.css"
+  },
+  {
+    "revision": "e78ab56f9afb86597373",
+    "url": "/js/app.9922a446.js"
+  },
+  {
+    "revision": "b8c7525841d619dca0ee",
+    "url": "/css/chunk-vendors.7f89301a.css"
+  },
+  {
+    "revision": "b8c7525841d619dca0ee",
+    "url": "/js/chunk-vendors.e1782ddb.js"
+  },
+  {
+    "revision": "291a639efe04f49d2b74",
+    "url": "/js/lang-hi-json.212bd0a8.js"
+  },
+  {
+    "revision": "ba30bf74627bd0805136",
+    "url": "/js/lang-mr-json.e16e93b4.js"
+  },
+  {
+    "revision": "4f842bbd3a220d972c6c",
+    "url": "/js/view-Cat-vue.7ceed156.js"
+  },
+  {
+    "revision": "1ad1a5c9d9d6063bb195adcae8d4e21e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d1b1c0fd447a75e6e60a61ca041aae",
+    "url": "/assets/plugins/bootstrap-4.1.2-dist/css/bootstrap.min.css"
+  },
+  {
+    "revision": "e27798d9322a4f4d293eabf9d1b92ed1",
+    "url": "/assets/tail-spin.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f92a3f337500984fbd20487501257dae",
+    "url": "/assets/plugins/bootstrap-4.1.2-dist/js/bootstrap.min.js"
+  }
+];
